5862b54b73db3aab516fafc3857834be49e6f61e
Kim Phuong Tu <kimphuongtu135@gmail.com>
2020-02-27 18:34:47 -0500

    Validation for register form- Done

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post("/login",(req,res)=>{
 
     if(req.body.email =="") 
     {
-            errorMessages.push({emailError:"Enter your email"});
+        errorMessages.push({emailError:"Enter your email"});
     }             
 
 
@@ -65,7 +65,10 @@ app.post("/login",(req,res)=>{
     {
         errorMessages.push({passError:"Enter your password"});
     }
+    
 
+    
+   
 
     //THere is an error
     if(errorMessages.length>0)
@@ -74,7 +77,7 @@ app.post("/login",(req,res)=>{
             title:"Login",
             messages:errorMessages  ,             
             email: req.body.email,         
-            password:req.body.password  
+            password:req.body.password 
                       
         })
     }
@@ -84,6 +87,65 @@ app.post("/login",(req,res)=>{
     {
         res.render("login",{
             title:"Login",
+            message:`We will contact ${req.body.email}` 
+         })
+    }
+    
+
+});
+
+app.post("/customer",(req,res)=>{    
+    const errorMessages=[];
+
+    if(req.body.yname =="") 
+    {
+        errorMessages.push({nameError:"Enter your name"});
+    }
+    else if (!/^[a-zA-Z]{2,}$/.test(`${req.body.yname}`))
+    {
+        errorMessages.push({nameError:"Enter your name with letter only"});
+    }   
+
+    if(req.body.email =="") 
+    {
+        errorMessages.push({emailError:"Enter your email"});
+    }             
+
+
+    if(req.body.password=="")
+    {
+        errorMessages.push({passError:"Enter your password"});
+    }
+    else if (!/^[a-zA-Z0-9]{6,12}$/.test(`${req.body.password}`))
+    {
+        errorMessages.push({passError:"Enter password between 6 to 12 characters with letter and number only"});
+    }
+
+     if(`${req.body.passwordagain}`!=`${req.body.password}`)
+    {
+        errorMessages.push({passagainError:"Password is not matching"});
+    }
+    
+
+
+    //THere is an error
+    if(errorMessages.length>0)
+    {
+        res.render("customer",{
+            title:"Customer",
+            messages:errorMessages  ,    
+            yname: req.body.yname,         
+            email: req.body.email,         
+            password:req.body.password  
+                      
+        })
+    }
+    
+    // there is no error
+    else
+    {
+        res.render("customer",{
+            title:"Customer",
             message:`We will contact ${req.body.email}` 
          })
     }
