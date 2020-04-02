3df187cda17df34bd3ef9482a108845b327b45eb
Kim Phuong Tu <kimphuongtu135@gmail.com>
2020-04-02 18:11:01 -0400

    Connected to MongoDB

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 require('dotenv').config({path:"./config/keys.env"});
 
 const app = express();
@@ -21,6 +22,13 @@ const productController=require("./controllers/product");
 
 app.use("/",generalController);
 app.use("/products",productController);
+
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log("Connected to MongoDB");
+})
+.catch(err=>console.log(`Error occured while connecting to database: ${err}`));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
