3c140018314fa359f295775bfd4c8581f67686af
Kim Phuong Tu <kimphuongtu135@gmail.com>
2020-04-09 21:40:48 -0400

    Created product database and be able to insert product into database

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-
 const productModel = require("../models/product");
+const isLogin=require("../middleware/auth");
+const productDBModel = require("../models/productData");
 
 router.get("/", (req, res) => {
     res.render("products/products",
@@ -11,6 +12,35 @@ router.get("/", (req, res) => {
 
         })
 
+});
+
+router.get("/add",isLogin,(req,res)=>
+{
+    res.render("products/productsAddForm",
+    {
+        title: "Add Products",
+        
+
+    })
+});
+
+router.post("/add",(req,res)=>
+{
+        const newProduct = {
+            productName : req.body.pname,
+            productPrice : req.body.pprice,
+            productDetail : req.body.pdetail,
+            productCategory  : req.body.pcate,
+            productQuantity : req.body.pquan,
+            bestSeller : req.body.pbest==='true'?true:false
+        }
+
+     const product =  new productDBModel(newProduct);
+     product.save()
+     .then(()=>{
+         res.redirect("/profile")
+     })
+     .catch(err=>console.log(`Error happened when inserting in the database :${err}`));
 });
 
 
---
+++ models/productData.js
@@ -0,0 +1,54 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productDBSchema = new Schema({
+    productName:
+    {
+        type: String,
+        required: true
+    },
+    productPrice:
+    {
+        type: Number,
+        required: true
+    },
+    productDetail:
+    {
+        type: String,
+        required: true
+    },
+    productCategory:
+    {
+        type: String,
+        required: true
+    },
+    productQuantity:
+    {
+        type: Number,
+        required: true
+    },
+    bestSeller:
+    {
+        type: Boolean,
+        default: "false"
+    },
+    productPic:
+    {
+        type:String
+    },
+    dateCreated:
+    {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+
+
+const productDB = mongoose.model('Product', productDBSchema);
+
+module.exports = productDB;
+
+
